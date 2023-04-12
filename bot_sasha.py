22786f816148cfb5c3bf1e7bc6b0a366b06f2975
Vitryanyuk, Sasha <90406739+PusheenTheCat5@users.noreply.github.com>
2023-04-11 22:42:03 -0700

    Create bot_sasha.py

---
+++ bot_sasha.py
@@ -0,0 +1,94 @@
+import random
+
+class Bot:
+    def get_decision(self, dealer_up_card, hand, dealer_prev_hand):
+        for card in dealer_up_card == 2:
+            if hand == 20, 10, 9, 19, 13, 14, 15, 16, 17, 18, 21:
+                return "stand"
+            elif hand == 10, 7, 9, 11:
+                return "double down"
+            elif hand == A A, 3 3, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 3:
+            if hand == 13, 14, 15, 16, 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 9, 10, 11:
+                return "double down"
+            elif hand == A A, 3 3, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 4:
+            if hand == 12, 13, 14, 15, 16, 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 9, 10, 11:
+                return "double down"
+            elif hand == A A, 3 3, 4 4, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 5:
+            if hand == 12, 13, 14, 15, 16, 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 9, 10, 11:
+                return "double down"
+            elif hand == A A, 3 3, 4 4, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 6:
+            if hand == 12, 13, 14, 15, 16, 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 9, 10, 11:
+                return "double down"
+            elif hand == A A, 3 3, 4 4, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 7:
+            if hand == 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 10, 11:
+                return "double down"
+            elif hand == A A, 3 3, 6 6, 7 7, 8 8:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 8:
+            if hand == 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 10, 11:
+                return "double down"
+            elif hand == A A, 6 6, 7 7, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 9:
+            if hand == 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 9, 10, 11:
+                return "double down"
+            elif hand == A A, 8 8, 9 9:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 10:
+            if hand == 14, 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 11:
+                return "double down"
+            elif hand == A A, 8 8:
+                return "split pairs"
+            else:
+                return "hit"
+        for card in dealer_up_card == 1 or 11:
+            if hand == 17, 18, 19, 20, 21:
+                return "stand"
+            elif hand == 11:
+                return "double down"
+            elif hand == A A, 8 8:
+                return "split pairs"
+            else:
+                return "hit"
